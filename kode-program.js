ec179d09992f58592e90ab98b6be0db8276874ea
masadamsahid <62916459+masadamsahid@users.noreply.github.com>
2021-04-27 15:16:14 +0700

    asdsad
    asdasd

---
+++ kode-program.js
@@ -0,0 +1,10 @@
+
+//Fungsi Log
+console.log("Ini contoh log")
+
+//Fungsi Alert
+alert("Hello, ini adalah program JS eksternal!")
+//bisa juga dengan window.alert("Halo, Jendela!")
+
+//Fungsi document.write()
+document.write(<h1>Ini Hasil Write</h1>)
