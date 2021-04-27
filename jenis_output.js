018ae0763fd727d194657f4b65dd2b386e99506a
masadamsahid <62916459+masadamsahid@users.noreply.github.com>
2021-04-27 16:34:21 +0700

    Commit ke 2
    Nmabahin Tugas 2

---
+++ jenis_output.js
@@ -0,0 +1,18 @@
+
+//✅✅✅ alert()
+function alertOutput(){
+    alert("Kamu telah mengaktifkan Alert!")
+}
+
+//✅✅✅ console.log()
+function consoleLogOutput(){
+    console.log("INI ADALAH HASIL CONSOLE LOG")
+}
+
+//✅✅✅ innerHTML
+function innerHTMLOutput(){
+    var inputan = document.getElementById("inputInnerHTML").value
+
+    document.getElementById("outputInnerHTML").innerHTML = inputan;
+
+}
